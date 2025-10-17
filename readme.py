16ac15fa98f927c40b2537d1ea762b5facad0d4d
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-17 11:36:14 +0530

    This is somthing new added in this repo

---
+++ readme.py
@@ -0,0 +1,9 @@
+# python-lab
+# A collection of Python code samples, mini projects, and logic-building exercises.
+collection = {1, 2, 3, 4, 5}
+print(collection)
+print(type(collection))
+
+collection = {}
+print(collection)
+print(type(collection))
