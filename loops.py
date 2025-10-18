cfece2a44b9b70e2bc4b9ddd2e82335619b52a63
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-18 13:00:31 +0530

    new commits

--- loops.py
+++ loops.py
@@ -40,3 +40,34 @@ i = 1
 while i <= 10:
     print(i*i)
     i += 1
+    heros = ["spider" , "ironman", "thor", "hulk", "captain america"]
+x = "hulk"
+ind = 0 
+while ind < len(heros):
+    if (heros[ind] == x):
+        print("found hulk")
+        break
+    else:
+        print("findind...")
+    ind += 1
+
+num  = [1,2,3,4,5,6,7,8,9,10]
+x =  4
+inx = 0
+while inx < len(num):
+    if (num[inx] == x):
+        print("found")
+        break    
+    else:
+#         print("not found")
+        inx += 1 
+
+i = 0 
+while i <= 5:
+        if (i == 3):
+         i +=1
+         continue
+        print(i)
+        i += 1
+        break
+
