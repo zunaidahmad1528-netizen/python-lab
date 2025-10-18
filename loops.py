14b06218a25029ed19281a7766532e2f5485944d
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-10-18 12:57:48 +0530

    loops code in python

---
+++ loops.py
@@ -0,0 +1,42 @@
+i = 1
+while i<=5:
+  
+    print("hello")
+#     i += 1
+i = 5
+while i >= 1:
+#     print(i)
+#     i -= 1
+
+ i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+      
+      
+i = 10 
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+
+i = 100 
+while i >= 1:
+    print(i)
+    i -= 1
+n = int(input("enter a number :"))
+i = 1 
+while i <= 10:
+    print(i*n)
+    i += 1
+
+i = 1
+while i <= 10:
+    print(i*i)
+    i += 1
