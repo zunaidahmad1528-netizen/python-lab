5d79bac4641cee089fe0a14325dc23bdd24b57ba
zunaidahmad1528-netizen <zunaidahmad1528@gmail.com>
2025-11-09 16:54:08 +0530

    New commit

--- loops.py
+++ loops.py
@@ -1,101 +1,104 @@
-i = 1
-while i<=5:
+# i = 1
+# while i<=5:
   
-    print("hello")
-#     i += 1
-i = 5
-while i >= 1:
+#     print("hello")
+# #     i += 1
+# i = 5
+# while i >= 1:
+# #     print(i)
+# #     i -= 1
+
+#  i = 1
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+      
+      
+# i = 10 
+# while i >= 1:
 #     print(i)
 #     i -= 1
 
- i = 1
-while i <= 10:
-    print(i)
-    i = i + 1
-      
-      
-i = 10 
-while i >= 1:
-    print(i)
-    i -= 1
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
 
 
-i = 1
-while i <= 100:
-    print(i)
-    i += 1
+# i = 100 
+# while i >= 1:
+#     print(i)
+#     i -= 1
+# n = int(input("enter a number :"))
+# i = 1 
+# while i <= 10:
+#     print(i*n)
+#     i += 1
 
+# i = 1
+# while i <= 10:
+#     print(i*i)
+#     i += 1
+#     heros = ["spider" , "ironman", "thor", "hulk", "captain america"]
+# x = "hulk"
+# ind = 0 
+# while ind < len(heros):
+#     if (heros[ind] == x):
+#         print("found hulk")
+#         break
+#     else:
+#         print("findind...")
+#     ind += 1
 
-i = 100 
-while i >= 1:
-    print(i)
-    i -= 1
-n = int(input("enter a number :"))
-i = 1 
-while i <= 10:
-    print(i*n)
-    i += 1
+# num  = [1,2,3,4,5,6,7,8,9,10]
+# x =  4
+# inx = 0
+# while inx < len(num):
+#     if (num[inx] == x):
+#         print("found")
+#         break    
+#     else:
+# #         print("not found")
+#         inx += 1 
 
-i = 1
-while i <= 10:
-    print(i*i)
-    i += 1
-    heros = ["spider" , "ironman", "thor", "hulk", "captain america"]
-x = "hulk"
-ind = 0 
-while ind < len(heros):
-    if (heros[ind] == x):
-        print("found hulk")
-        break
-    else:
-        print("findind...")
-    ind += 1
-
-num  = [1,2,3,4,5,6,7,8,9,10]
-x =  4
-inx = 0
-while inx < len(num):
-    if (num[inx] == x):
-        print("found")
-        break    
-    else:
-#         print("not found")
-        inx += 1 
-
-i = 0 
-while i <= 5:
-        if (i == 3):
-         i +=1
-         continue
-        print(i)
-        i += 1
-        break
-
-for i in range(34):
-        if (i==30):
-         break
-        print(i)
-        
-        
-# continue 
-for  i in range(12):
-        if i==0:
-                continue
+# i = 0 
+# while i <= 5:
+#         if (i == 3):
+#          i +=1
+#          continue
 #         print(i)
-i = 0
-while i <= 12:
-        i += 1
-        if i==6:
-                continue
-        elif i==8:
-                continue
-        
-        if i==11:
-                break
-        print(i)  
+#         i += 1
+#         break
 
-# for loop 
-for i in range(10):
-        print(i)
-for i in range(1,11):
-        print(i)
+# for i in range(34):
+#         if (i==30):
+#          break
+#         print(i)
+        
+        
+# # continue 
+# for  i in range(12):
+#         if i==0:
+#                 continue
+# #         print(i)
+# i = 0
+# while i <= 12:
+    
+#         i += 1
+#         if i==6:
+#                 continue
+#         elif i==8:
+#                 continue
+        
+#         if i==11:
+#                 break
+#         print(i)  
+
+# # for loop 
+# for i in range(10):
+    
+    
+#         print(i)
+# for i in range(1,11):
+#         print(i)
